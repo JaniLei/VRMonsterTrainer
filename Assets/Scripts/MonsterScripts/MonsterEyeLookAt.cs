dce407973b477717c29465c2cac3c1ee556796c4
AJ-Ora <antti-jussioravainen@kamk.fi>
2018-02-28 14:10:21 +0200

    Modified monster child animator controller. Added a simple script to control eyes for testing purposes.

---
+++ Assets/Scripts/MonsterScripts/MonsterEyeLookAt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterEyeLookAt : MonoBehaviour {
+
+	public Transform eyeLeft;
+	public Transform eyeRight;
+	public Transform lookAtTarget;
+
+	void Start ()
+	{
+		if (eyeLeft == null || eyeRight == null || lookAtTarget == null)
+		{
+			this.enabled = false;
+		}
+	}
+	
+	void Update ()
+	{
+		eyeLeft.LookAt (lookAtTarget);
+		eyeRight.LookAt (lookAtTarget);
+	}
+}
