6d25a4feaf70f40793db1caf692aa33149a360e4
Leinonen Jani TTK16SP <janileinonen1@kamk.fi>
2018-04-05 10:28:05 +0300

    produce tree modified

--- Assets/Scripts/FruitTree.cs
+++ Assets/Scripts/FruitTree.cs
@@ -5,37 +5,54 @@ using UnityEngine;
 public class FruitTree : MonoBehaviour
 {
     public float growTime = 60;
-    public GameObject fruitPrefab;
-    public Vector3 spawnPos;
+    public GameObject producePrefab;
+    public int produceAmount;
+    public Vector3[] spawnPos;
 
-    Valve.VR.InteractionSystem.Edible fruit;
-    Vector3 fullScale;
+    struct Produce
+    {
+        public Valve.VR.InteractionSystem.Edible edible;
+        public Vector3 fullScale;
+        public Vector3 startPos;
+    }
+    Produce[] produce;
     float currentTime;
 
 	void Start()
     {
-        fullScale = fruitPrefab.transform.localScale;
-        SpawnFruit();
+        produce = new Produce[produceAmount];
+        SpawnProduce();
+        for (int i = 0; i < produce.Length; i++)
+        {
+            produce[i].fullScale = producePrefab.transform.localScale;
+            produce[i].startPos = produce[i].edible.transform.position;
+        }
         //StartCoroutine("GrowFruit");
-	}
+    }
 
     void Update()
     {
         currentTime += Time.deltaTime;
-        if (fruit)
+
+        for (int i = 0; i < produce.Length; i++)
         {
-            if (fruit.transform.localScale.x < fullScale.x &&
-                fruit.transform.localScale.y < fullScale.y &&
-                fruit.transform.localScale.z < fullScale.z)
+            Produce prod = produce[i];
+            if (prod.edible)
             {
-                float growth = currentTime / growTime;
-                Vector3 scaleGrowth = new Vector3(growth * fullScale.x, growth * fullScale.y, growth * fullScale.z);
-                fruit.transform.localScale = scaleGrowth;
-            }
-            else
-            {
-                fruit.gameObject.layer = LayerMask.NameToLayer("Default");
-                fruit = null;
+                if (prod.edible.transform.localScale.x < prod.fullScale.x &&
+                    prod.edible.transform.localScale.y < prod.fullScale.y &&
+                    prod.edible.transform.localScale.z < prod.fullScale.z)
+                {
+                    float growth = currentTime / growTime;
+                    Vector3 scaleGrowth = new Vector3(growth * prod.fullScale.x, growth * prod.fullScale.y, growth * prod.fullScale.z);
+                    prod.edible.transform.localScale = scaleGrowth;
+                    prod.edible.transform.position = prod.startPos + new Vector3(0, -(growth / 3), 0);
+                }
+                else
+                {
+                    prod.edible.gameObject.layer = LayerMask.NameToLayer("Default");
+                    prod.edible = null;
+                }
             }
         }
     }
@@ -62,17 +79,26 @@ public class FruitTree : MonoBehaviour
     //    //}
     //}
 
-    void SpawnFruit()
+    void SpawnProduce()
     {
-        fruit = Instantiate(fruitPrefab, transform.position + spawnPos, fruitPrefab.transform.rotation, transform).GetComponent<Valve.VR.InteractionSystem.Edible>();
-        fruit.gameObject.layer = LayerMask.NameToLayer("IgnoreHand");
-        fruit.transform.localScale = Vector3.zero;
-        fruit.GetComponent<Rigidbody>().isKinematic = true;
+        for (int i = 0; i < produce.Length; i++)
+        {
+            produce[i].edible = Instantiate(producePrefab, transform.position + spawnPos[i], producePrefab.transform.rotation, transform).GetComponent<Valve.VR.InteractionSystem.Edible>();
+            produce[i].edible.gameObject.layer = LayerMask.NameToLayer("IgnoreHand");
+            produce[i].edible.transform.localScale = Vector3.zero;
+            produce[i].edible.GetComponent<Rigidbody>().isKinematic = true;
+        }
     }
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(transform.position + spawnPos, 0.05f);
+        if (spawnPos.Length > 0)
+        {
+            Gizmos.color = Color.red;
+            for (int i = 0; i < spawnPos.Length; i++)
+            {
+                Gizmos.DrawSphere(transform.position + spawnPos[i], 0.05f);
+            }
+        }
     }
 }
