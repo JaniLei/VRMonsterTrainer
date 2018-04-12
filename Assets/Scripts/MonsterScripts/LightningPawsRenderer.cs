ebac2efd7552e97c85c31b768c3d8ae398e74dd0
AJ-Ora <antti-jussioravainen@kamk.fi>
2018-04-12 15:14:00 +0300

    Modified monster hand pointing animation. Updated lightning paws renderer and added new particle effects to make the lightning effect cooler.

--- Assets/Scripts/MonsterScripts/LightningPawsRenderer.cs
+++ Assets/Scripts/MonsterScripts/LightningPawsRenderer.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class LightningPawsRenderer : MonoBehaviour
 {
 	private LineRenderer line;
+	private Material mat;
+
 	[SerializeField] private Transform[] bones;
+	[SerializeField] private bool wiggle = false;
+	[SerializeField][Range(0.0f,0.1f)] private float wiggleStrength = 0.0f;
+	[SerializeField] private bool offsetTexture = false;
+	[SerializeField][Range(0.0f,1.0f)] private float offsetStrength = 0.0f;
 
 	void Start ()
 	{
@@ -41,13 +47,31 @@ public class LightningPawsRenderer : MonoBehaviour
 		{
 			line.enabled = true;
 		}
+
+		//Get the line renderer's material.
+		mat = line.material;
 	}
 	
 	void Update ()
 	{
+		//Move line renderer's points to bone positions.
 		for(int i = 0; i < bones.Length; i++)
 		{
-			line.SetPosition (i, bones[i].position);
+			Vector3 wigglePosition = Vector3.zero;
+			if (wiggle)
+			{
+				if (i != 0 && i != bones.Length - 1)
+				{
+					wigglePosition = new Vector3 (Random.Range (-wiggleStrength, wiggleStrength), Random.Range (-wiggleStrength, wiggleStrength), Random.Range (-wiggleStrength, wiggleStrength));
+				}
+			}
+			line.SetPosition (i, bones[i].position + wigglePosition);
+		}
+
+		//Apply material offset.
+		if (offsetTexture)
+		{
+			mat.mainTextureOffset = new Vector2 (Random.Range (-offsetStrength, offsetStrength), 0.0f);
 		}
 	}
 }
