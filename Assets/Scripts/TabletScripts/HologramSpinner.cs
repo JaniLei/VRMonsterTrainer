1c056b04d33423aaeb040a661d0b003f82425e0b
AJ-Ora <antti-jussioravainen@kamk.fi>
2018-05-17 14:05:26 +0300

    Created UI layout for tablet.

---
+++ Assets/Scripts/TabletScripts/HologramSpinner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HologramSpinner : MonoBehaviour {
+
+	[SerializeField] private float spinSpeed = 0.0f;
+	[SerializeField] private bool rotateZ = false;
+
+	void Update ()
+	{
+		if (!rotateZ)
+		{
+			Vector3 rotation = new Vector3 (0.0f, spinSpeed * Time.deltaTime, 0.0f);
+			transform.localEulerAngles += rotation;
+		}
+		else
+		{
+			Vector3 rotation = new Vector3 (0.0f, 0.0f, spinSpeed * Time.deltaTime);
+			transform.localEulerAngles += rotation;
+		}
+	}
+}
