2e23085a5288ae8c7fc57da83a9ac6d8d9e945bc
Lauri <laurihei97@gmail.com>
2018-05-18 10:04:32 +0300

    egg model update

---
+++ Assets/Scripts/Wetness.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wetness : MonoBehaviour
+{
+
+    public MeshRenderer Wet;
+    public float GlossyValue = 2.1f;
+    public float DefaultValue = 0.25f;
+
+    public bool isWet;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWet)
+        {
+            Wet.material.SetFloat("_Glossiness", GlossyValue);
+        }
+        else
+        {
+            Wet.material.SetFloat("_Glossiness", GlossyValue);
+        }
+
+    }
+}
